1e7f2f4b1bd533dca157e5d51770665579f0dfb1
David Mah <MahHaha@gmail.com>
2013-04-12 09:42:10 -0700

    Initial Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eduki::Application.config.secret_token = '1ddaa0d5d2831d616f857aca64974f7486147a6ef0708c58662f5868ba5d10d694048fa9e8313e915ea43e376484df5e8ca05d347b9cccf66b28dceacf175853'
