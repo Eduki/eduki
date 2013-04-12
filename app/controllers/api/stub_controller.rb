4126cd0d5a98b6c07eae1b97855363990f735941
David Mah <MahHaha@gmail.com>
2013-04-12 10:03:22 -0700

    Database user model stub created

--- app/controllers/api/stub_controller.rb
+++ app/controllers/api/stub_controller.rb
@@ -1,5 +1,8 @@
 class Api::StubController < ApplicationController
   def index
-    render :json => {:derp => 'herp'}
+    a = User.new
+    a.email = "herp"
+    a.save
+    render :json => {:derp => a.email, :id => a.id}
   end
 end
