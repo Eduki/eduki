7ea3fade882266113a98c78521062d22a4660023
abboudm <abboud.mic@gmail.com>
2013-05-03 15:55:23 -0700

    added server save code

--- app/assets/javascripts/create-lesson.js
+++ app/assets/javascripts/create-lesson.js
@@ -1,17 +1,42 @@
 $(document).ready(function() {
 	$("#les_button").click(makeAndSend);
+	var ajax = new XMLHttpRequest();
+	ajax.onload = fn;
+	ajax.onerror = errorFunction;
+	ajax.open("GET", "http://eduki.herokuapp.com/api/courses/1/lessons", true);
+	ajax.send();
 });
 
 var newLesson = Backbone.Model.extend({
-	urlRoot: '/courses',
+	urlRoot: 'http://eduki.herokuapp.com/api/courses/1/lessons',
 
 	defaults: {
-		name: 'helo',
-		content: 'whhfdkslsk'
+		title: 'helo',
+		body: 'whhfdkslsk'
 	}
 });
 
 function makeAndSend(event) {
 	var course = new newLesson({name: $('#create-lesson-name').val(), content: $('#create-lesson-content').val()});
-	alert('course name: ' + course.get('name') + ',\ncourse content: ' + course.get('content'));
+	course.save({}, {              // generate POST /users - content: {name: 'John'}
+     success: function() {
+     		alert('course saved successfully');
+     },
+
+     error: function() {
+     		alert('course failed');
+     }
+ }); 
+}
+
+function fn(e) {
+  if (this.status == 200) {   // 200 means request succeeded
+    console.log(this.responseText);
+  } else {
+    console.log('error');
+  }
+}
+
+function errorFunction(e) {
+	console.log('error');
 }
