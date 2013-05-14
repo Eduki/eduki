6df31876ee4e70113f442a6a89a20fecf8d3c28a
David Mah <MahHaha@gmail.com>
2013-05-14 00:47:33 -0700

    Stubs from rails generate controller api/quiz

---
+++ spec/controllers/api/quiz_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Api::QuizController do
+
+  describe "GET 'show'" do
+    it "returns http success" do
+      get 'show'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'create'" do
+    it "returns http success" do
+      get 'create'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'update'" do
+    it "returns http success" do
+      get 'update'
+      response.should be_success
+    end
+  end
+
+end
