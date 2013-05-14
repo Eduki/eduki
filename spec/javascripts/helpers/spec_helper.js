88eb72e0bc95fadc3ef1fad8d4dc99181b01cebe
David Mah <MahHaha@gmail.com>
2013-05-14 00:02:46 -0700

    Reduced testing redundancy into spec_helper
    * FakeServer setup moved
    * Server Response logic moved

--- spec/javascripts/helpers/spec_helper.js
+++ spec/javascripts/helpers/spec_helper.js
@@ -4,9 +4,35 @@
  */
 //= require application.js
 
+var fixtures;
+// Setup Fixture Data for tests to reference
 function setupFixtures() {
-  jasmine.getFixtures().fixturesPath = "assets/fixtures"
-  jasmine.getJSONFixtures().fixturesPath = "assets/fixtures/json"
+  jasmine.getFixtures().fixturesPath = "assets/fixtures";
+  jasmine.getJSONFixtures().fixturesPath = "assets/fixtures/json";
+  fixtures = loadJSONFixtures("course.json", "courses.json");
+}
+setupFixtures();
+
+// Setup Fake Server to respond to AJAX requests
+// Should be called from the context of a test
+function setupFakeServer() {
+  beforeEach(function() {
+    this.server = sinon.fakeServer.create();
+    this.server.answeredIndex = 0;
+  });
+  afterEach(function() {
+    this.server.restore();
+  });
 }
 
-setupFixtures();
+// Responds to the least recently made but still unanswered
+// ajax request in server with a HTTP of code and the object in data.
+// Should be called from the context of a test
+function serverRespond(server, code, data) {
+  server.requests[server.answeredIndex].respond(
+    code,
+    {"Content-Type": "application/json"},
+    JSON.stringify(data)
+  );
+  server.answeredIndex += 1;
+}
--- spec/javascripts/views/course_spec.js
+++ spec/javascripts/views/course_spec.js
@@ -2,48 +2,22 @@
 // David Mah
 describe('Course', function() {
   describe("Index", function() {
-    var server;
-    // Setup Fake Server to respond to AJAX requests
-    beforeEach(function() {
-      server = sinon.fakeServer.create();
-    });
-    afterEach(function() {
-      server.restore();
-    });
-
+    setupFakeServer();
     it("renders zero courses", function() {
       var view = new Eduki.Views.CoursesIndex();
-
-      server.requests[0].respond(
-        200,
-        {"Content-Type": "application/json"},
-        JSON.stringify([])
-      );
-
+      serverRespond(this.server, 200, []);
       expect(view.$el).not.toContain("li");
     });
 
     it("renders one course", function() {
       var view = new Eduki.Views.CoursesIndex();
-
-      server.requests[0].respond(
-        200,
-        {"Content-Type": "application/json"},
-        JSON.stringify([{"id":1, "title":"Bear Cooking"}])
-      );
-
+      serverRespond(this.server, 200, fixtures["course.json"])
       expect(view.$el.find('li')).toHaveText("Bear Cooking");
     });
 
     it("renders many courses", function() {
       var view = new Eduki.Views.CoursesIndex();
-
-      server.requests[0].respond(
-        200,
-        {"Content-Type": "application/json"},
-        JSON.stringify([{"id":1, "title":"Bear Cooking"},
-                        {"id":2, "title":"Petting Zoos"}])
-      );
+      serverRespond(this.server, 200, fixtures["courses.json"]);
       list_items = view.$el.find('li');
       expect(list_items[0]).toHaveText("Bear Cooking");
       expect(list_items[1]).toHaveText("Petting Zoos");
