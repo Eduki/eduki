29be487b28e345333fa1fa5f20b663a0670d3184
David Mah <MahHaha@gmail.com>
2013-05-14 10:55:45 -0700

    Tests for creating/updating quizzes with problems included

--- spec/controllers/api/quiz_controller_spec.rb
+++ spec/controllers/api/quiz_controller_spec.rb
@@ -31,6 +31,24 @@ describe Api::QuizController do
     @quiz_three.title = "quiz_three title"
     @quiz_three.course = @course_two
     @quiz_three.save
+
+    @problem = Problem.new
+    @problem.question = "problem_one question"
+    @problem.answer = "problem_one answer"
+    @problem.quiz = @quiz
+    @problem.save
+
+    @problem_two = Problem.new
+    @problem_two.question = "problem_two question"
+    @problem_two.answer = "problem_two answer"
+    @problem_two.quiz = @quiz
+    @problem_two.save
+
+    @problem_three = Problem.new
+    @problem_three.question = "problem_three question"
+    @problem_three.answer = "problem_three answer"
+    @problem_three.quiz = @quiz_two
+    @problem_three.save
   end
 
   describe "GET #show" do
@@ -42,6 +60,16 @@ describe Api::QuizController do
       body['id'].should        == @quiz.id
       body['title'].should     == @quiz.title
       body['course_id'].should == @quiz.course_id
+    end
+
+    it "includes nested problems" do
+      get :show, :id => @quiz.id
+      assert_response :success
+      body = JSON.parse(response.body)
+      problems = body['problems']
+      problems.size.should == 2
+      problems[0]['id'] = @problem.id
+      problems[1]['id'] = @problem_two.id
     end
 
     it "returns 404 if id not found" do
@@ -60,6 +88,13 @@ describe Api::QuizController do
       body.size.should == 2
     end
 
+    it "should not include nested problems" do
+      get :index, :course_id => @course.id
+      assert_response :success
+      body = JSON.parse(response.body)
+      body['problems'].should be_nil
+    end
+
     it "returns 404 if course_id not found" do
       get :index, :course_id => -1
       check_failure(404)
@@ -72,13 +107,40 @@ describe Api::QuizController do
       post :create, :course_id => @course.id,
         :title => "quiz_four title"
       assert_response :success
+
+      # Response should have updated version
       body = JSON.parse(response.body)
       body['id'].should_not == @quiz.id
       body['id'].should_not == @quiz_two.id
       body['id'].should_not == @quiz_three.id
-      body['title'].should == "quiz_four title"
+      body['title'].should    == "quiz_four title"
+      body['problems'].should == []
+
+      # DB should be updated
       Quiz.count.should == (previous_size + 1)
       Quiz.last.title.should == "quiz_four title"
+    end
+
+    it "creates problems if problems are included" do
+      post :create, :course_id => @course.id,
+        :title => "quiz_four title",
+        :problems => [{:question=>"question_one", :answer=>"answer_one"},
+                      {:question=>"question_two", :answer=>"question_two"}]
+      assert_response :success
+      body = JSON.parse(response.body)
+
+      # Response should have updated version
+      body['problems'][0]['question'].should == "question_one"
+      body['problems'][1]['question'].should == "question_two"
+      body['problems'][0]['answer'].should   == "answer_one"
+      body['problems'][1]['answer'].should   == "answer_two"
+
+      # DB should be updated
+      Quiz.last.problems.size.should == 2
+      Quiz.last.problems[0].question.should == "question_one"
+      Quiz.last.problems[1].question.should == "question_two"
+      Quiz.last.problems[0].answer.should == "answer_one"
+      Quiz.last.problems[1].answer.should == "answer_two"
     end
 
     it "returns 404 if course_id not found" do
@@ -109,6 +171,30 @@ describe Api::QuizController do
       body = JSON.parse(response.body)
       body['title'].should == @quiz.title
       Quiz.find(@quiz.id).title.should == @quiz.title
+    end
+
+    it "replaces the full problem set if problems are included" do
+      put :update, :id => @quiz_two.id,
+        :problems => [{:question=>"question_one", :answer=>"answer_one"},
+                      {:question=>"question_two", :answer=>"question_two"}]
+      assert_response :success
+
+      # Response should have updated version
+      body = JSON.parse(response.body)
+      body['problems'][0]['question'].should == "question_one"
+      body['problems'][1]['question'].should == "question_two"
+      body['problems'][0]['answer'].should   == "answer_one"
+      body['problems'][1]['answer'].should   == "answer_two"
+
+      # DB should be updated
+      Quiz.last.problems.size.should == 2
+      Quiz.last.problems[0].question.should == "question_one"
+      Quiz.last.problems[1].question.should == "question_two"
+      Quiz.last.problems[0].answer.should == "answer_one"
+      Quiz.last.problems[1].answer.should == "answer_two"
+
+      # Old problem records should be destroyed
+      expect { Problem.find_by_id(@problem_three.id) }.to raise_error
     end
 
     it "returns 404 if id not found" do
