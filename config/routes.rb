1546e3052946da1713f0fe666e4088427607fbc2
David Mah <MahHaha@gmail.com>
2013-05-14 11:11:35 -0700

    Shallow routes on course and lessons
    * Added routes for lessons to be accessed without a course id
    * Pruned routes that weren't being used

--- config/routes.rb
+++ config/routes.rb
@@ -56,14 +56,14 @@ Eduki::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  #   match 'products/:id' => 'catalog#view'
   apipie
   root :to => 'static#index'
   match 'api' => 'api/stub#index'
-  #   match 'products/:id' => 'catalog#view'
   namespace :api do
-    resources :courses do
-      resources :lessons
+    resources :courses, :only => [:show, :index, :create, :update] do
+      resources :lessons, :only => [:show, :index, :create, :update]
     end
+    resources :lessons, :only => [:create, :update]
   end
-
 end
