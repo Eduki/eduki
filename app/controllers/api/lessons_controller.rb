ee44402b19d925a3bbbb31c6907fb05f474699b8
David Mah <MahHaha@gmail.com>
2013-05-14 11:27:20 -0700

    Refactored course and lesson tests and routes
    * Changed lesson routes to include shallow versions
    * Lesson controller doesn't check for consistent courses anymore
    * Moved check_failure helper function to spec_helper
    * Add some light comments to course and lesson specs

--- app/controllers/api/lessons_controller.rb
+++ app/controllers/api/lessons_controller.rb
@@ -5,9 +5,8 @@ class Api::LessonsController < Api::ApiController
 
   # For all methods, assume that
   # @course and @lesson have been retrieved if applicable
-  before_filter :get_course_or_404
+  before_filter :get_course_or_404, :only => [:index, :create]
   before_filter :get_lesson_or_404, :only => [:show, :update]
-  before_filter :check_course_matches_lesson, :only => [:show, :update]
 
   resource_description do
     description <<-EOS
@@ -70,18 +69,5 @@ private
 
   def get_lesson_or_404
     super(params[:id])
-  end
-
-  # Checks of @lesson is indeed a member of @course
-  # Returns false and renders 404 if that isn't the case.
-  # Meant to be used in before_filters
-  def check_course_matches_lesson
-    if @lesson.course != @course
-      err = error_object
-      err[:message] =
-        "Lesson of ID=#{@lesson.id} not a member of Course of ID=#{@course.id}"
-      render :json => err, :status => 404
-      return false
-    end
   end
 end
--- spec/controllers/api/courses_controller_spec.rb
+++ spec/controllers/api/courses_controller_spec.rb
@@ -29,9 +29,7 @@ describe Api::CoursesController do
 
     it "returns 404 if id not found" do
       get :show, :id => -1
-      assert_response 404
-      body = JSON.parse(response.body)
-      body['error'].should_not == nil
+      check_failure(404)
     end
   end
 
@@ -39,6 +37,8 @@ describe Api::CoursesController do
     it "shows all courses" do
       get :index
       assert_response :success
+
+      # Response should have proper version
       body = JSON.parse(response.body)
       body[0]['title'].should == "course example"
       body[1]['title'].should == "course_two example"
@@ -50,17 +50,19 @@ describe Api::CoursesController do
       previous_size = Course.count
       post :create, :title => "course_three example"
       assert_response :success
+
+      # Response should have proper version
       body = JSON.parse(response.body)
       @course.id.should_not == body['id']
       @course_two.id.should_not == body['id']
+
+      # DB should be updated
       Course.count.should == (previous_size + 1)
     end
 
     it "returns 400 if title missing" do
       post :create
-      assert_response 400
-      body = JSON.parse(response.body)
-      body['error'].should_not == nil
+      check_failure(400)
     end
   end
 
@@ -69,25 +71,31 @@ describe Api::CoursesController do
     it "updates 1 Course" do
       put :update, :id => @course.id, :title => "course title change"
       assert_response :success
+
+      # Response should have proper version
       body = JSON.parse(response.body)
       @course.id.should == body['id']
       body['title'].should == "course title change"
+
+      # DB should be updated
       Course.find(@course.id).title.should == "course title change"
     end
 
     it "updates nothing if nothing included" do
       put :update, :id => @course.id, :id => @course.id
+
+      # Response should have proper version
       assert_response :success
       body = JSON.parse(response.body)
       body['title'].should == @course.title
+
+      # DB should be updated
       Course.find(@course.id).title.should == @course.title
     end
 
     it "returns 404 if id not found" do
       put :update, :id => -1, :title => "course title change"
-      assert_response 404
-      body = JSON.parse(response.body)
-      body['error'].should_not == nil
+      check_failure(404)
     end
 
   end
