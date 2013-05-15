a7f4bcc3fd1552fe8ec0076ad2f7bed744cbb6c0
David Mah <MahHaha@gmail.com>
2013-05-14 19:13:09 -0700

    Reduced resource 404 code redundancy

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -10,33 +10,28 @@ protected
   # instance variable @course. Returns false and renders 404 if not found
   # Meant to be used in before_filters
   def get_course_or_404(id)
-    @course = Course.find_by_id(id)
-    if @course.nil?
-      render :json => Course.missing_course(:id), :status => 404
-      return false
-    end
+    @course = get_or_404(Course, id)
   end
 
   # Retrieves the lesson with the given id and assigns it to the
   # instance variable @lesson. Returns false and renders 404 if not found
   # Meant to be used in before_filters
   def get_lesson_or_404(id)
-    @lesson = Lesson.find_by_id(id)
-    if @lesson.nil?
-      render :json => Lesson.missing_lesson(:id), :status => 404
-      return false
-    end
+    @lesson = get_or_404(Lesson, id)
   end
 
-  # Retrieves the lesson with the given id and assigns it to the
-  # instance variable @lesson. Returns false and renders 404 if not found
+  # Retrieves the quiz with the given id and assigns it to the
+  # instance variable @quiz. Returns false and renders 404 if not found
   # Meant to be used in before_filters
   def get_quiz_or_404(id)
-    @quiz = Quiz.find_by_id(id)
-    if @quiz.nil?
-      render :json => Quiz.missing_quiz(:id), :status => 404
-      return false
-    end
+    @quiz = get_or_404(Quiz, id)
+  end
+
+  # Retrieves the problem with the given id and assigns it to the
+  # instance variable @problem. Returns false and renders 404 if not found
+  # Meant to be used in before_filters
+  def get_problem_or_404(id)
+    @problem = get_or_404(Problem, id)
   end
 
   # A hash that is the base for any errors
@@ -44,4 +39,16 @@ protected
   def error_object
     {:error => "Error encountered"}
   end
+
+private
+
+  def get_or_404(type, id)
+    obj = type.find_by_id(id)
+    if obj.nil?
+      render :json => type.missing_instance(id), :status => 404
+      return nil
+    end
+    return obj
+  end
+
 end
--- app/controllers/api/quizzes_controller.rb
+++ app/controllers/api/quizzes_controller.rb
@@ -20,19 +20,19 @@ class Api::QuizzesController < Api::ApiController
     EOS
   end
 
-  api :GET, '/quizzes/:id'
+  api :GET, '/quizzes/:id', "Retrieve a quiz"
   param :id, Fixnum, :required => true
   def show
     render :json => @quiz
   end
 
-  api :GET, '/courses/:course_id/quizzes'
+  api :GET, '/courses/:course_id/quizzes', "Retrieve a list of quizzes"
   param :course_id, Fixnum, :required => true
   def index
     render :json => Quiz.find_all_by_course_id(@course.id), :each_serializer => QuizListingSerializer
   end
 
-  api :POST, '/courses/:course_id/quizzes'
+  api :POST, '/courses/:course_id/quizzes', "Create a quiz"
   param :course_id, Fixnum, :required => true
   param :title, String, :required => true
   param :problems, Array, :desc => "A list of JSON objects of { question:string, answer:string }"
@@ -45,34 +45,18 @@ class Api::QuizzesController < Api::ApiController
       @quiz = Quiz.new
       @quiz.title = params[:title]
       @quiz.course = @course
-      new_problems = []
 
       # If problems were included (they're optional), take each problem hash, create a problem object
       # Stuff all of these problem objects into the quiz
-      if not params[:problems].nil?
-        problem_hashes = params[:problems]
-        problem_hashes.each do |problem_hash|
-          problem = Problem.create_from_hash(problem_hash)
-          problem.quiz = @quiz
-
-          # Fail if a problem is invalidly formatted
-          if problem.nil?
-            err = error_object
-            err[:message] = "A problem hash was invalid"
-            render :json => err, :status => 400
-            return false
-          end
-
-          new_problems << problem
-        end
-      end
+      new_problems = create_new_problems_list(params[:problems])
+      return false if new_problems.nil?
       @quiz.problems = new_problems
       @quiz.save
       render :json => @quiz
     end
   end
 
-  api :PUT, '/quizzes/:id'
+  api :PUT, '/quizzes/:id', "Update a quiz"
   param :id,        Fixnum, :required => true
   param :course_id, Fixnum
   param :title,     String
@@ -84,25 +68,13 @@ class Api::QuizzesController < Api::ApiController
     # If problems were included (they're optional), take each problem hash, create a problem object
     # Stuff all of these problem objects into the quiz
     if not params[:problems].nil?
-      problem_hashes = params[:problems]
-      new_problems = []
-      problem_hashes.each do |problem_hash|
-        problem = Problem.create_from_hash(problem_hash)
-        problem.quiz = @quiz
+      new_problems = create_new_problems_list(params[:problems])
+      return false if new_problems.nil?
 
-        # Fail if a problem is invalidly formatted
-        if problem.nil?
-          err = error_object
-          err[:message] = "A problem hash was invalid"
-          render :json => err, :status => 400
-          return false
-        end
-
-        new_problems << problem
-      end
-
+      # Destroy the old problems
       old_problems   = @quiz.problems
       old_problems.each { |problem| problem.destroy }
+
       @quiz.problems = new_problems
       new_problems.each { |problem| problem.save }
     end
@@ -119,4 +91,29 @@ private
     super(params[:id])
   end
 
+  # Creates and returns a list of problem objects given the problems data
+  # Problem objects are not saved to the database
+  # Format of problems_data is a list of hashes, where each hash is
+  # {'question':string, 'answer':string}
+  # Returns nil if input is incorrectly formatted
+  def create_new_problems_list(problem_hashes)
+    new_problems = []
+    if not problem_hashes.nil?
+      problem_hashes.each do |problem_hash|
+        problem = Problem.create_from_hash(problem_hash)
+        problem.quiz = @quiz
+
+        # Fail if a problem is invalidly formatted
+        if problem.nil?
+          err = error_object
+          err[:message] = "A problem hash was invalid"
+          render :json => err, :status => 400
+          return nil
+        end
+
+        new_problems << problem
+      end
+    end
+    return new_problems
+  end
 end
