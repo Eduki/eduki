b0357fa634a6a7a248fb748c8abd56ebeb850525
abboudm <abboud.mic@gmail.com>
2013-05-16 23:53:29 -0700

    added quiz route and quiz creation template

--- app/assets/javascripts/routers/courses_router.js
+++ app/assets/javascripts/routers/courses_router.js
@@ -8,7 +8,8 @@ Eduki.Routers.Courses = Backbone.Router.extend({
   routes: {
     'courses(/)': 'index',
     'courses/new': 'create',
-    'courses/:cid(/)': 'overview'
+    'courses/:cid(/)': 'overview',
+    'courses/:cid/quiz-new(/)': 'new_quiz'
   },
 
   index: function() {
@@ -26,6 +27,11 @@ Eduki.Routers.Courses = Backbone.Router.extend({
   overview: function(cid) {
     // Delegate to the CoursesOverview View and render it inside of the container
     var view = new Eduki.Views.CoursesOverview({attributes:{course_id: cid}});
+    $('#main-content').html(view.render().el);
+  },
+
+  new_quiz: function(cid) {
+    var view = new Eduki.Views.QuizNew({attributes:{course_id: cid}});
     $('#main-content').html(view.render().el);
   }
 
