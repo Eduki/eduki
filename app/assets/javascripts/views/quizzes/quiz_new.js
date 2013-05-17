01eb20ac733b05005a61e28ecf90f1abd8b6fb6e
abboudm <abboud.mic@gmail.com>
2013-05-16 23:54:32 -0700

    added quiz view with dummy functionality

---
+++ app/assets/javascripts/views/quizzes/quiz_new.js
@@ -0,0 +1,31 @@
+Eduki.Views.QuizNew = Backbone.View.extend({
+	
+	template: JST['quizzes/create_quiz'],
+	errorTemplate: JST['static/error'],
+
+	events: {
+		'click button#add': 'add',
+		'submit form': 'submit'
+	},
+
+	initialize: function(cid) {
+    this.render(this.template());
+  },
+
+    // Renders the template
+  render: function(template) {
+    $(this.el).html(template);
+    return this;
+  },
+
+  add: function(e) {
+  	e.preventDefault();
+  	$('#questions').append($('#question').clone());
+  },
+
+  submit: function(e) {
+  	e.preventDefault();
+  	alert('event fired!' + e.target);
+  } 
+
+});
