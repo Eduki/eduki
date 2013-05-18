b1df96d2e5cdb1aca0ea8ff475a2445735b5adb8
Jolie Chen <omjolie@gmail.com>
2013-05-18 15:24:21 -0700

    Styiling and Templates

--- app/assets/javascripts/views/signup/signup.js
+++ app/assets/javascripts/views/signup/signup.js
@@ -9,9 +9,9 @@
 
 Eduki.Views.SignupIndex = Backbone.View.extend({
 	
-	template: JST['users/signup'],
-	successTemplate: JST['users/signup_success'],
-	errorTemplate: JST['users/signup_error'],
+	template: JST['signup/signup'],
+	successTemplate: JST['signup/signup_success'],
+	errorTemplate: JST['signup/signup_error'],
 
 	events: {
 		'submit form' : 'signup'
@@ -29,15 +29,20 @@ Eduki.Views.SignupIndex = Backbone.View.extend({
 	 // handles the form submission, displays appropriate pages on success/error
   signup: function(e) {
     e.preventDefault();
-    this.user = new Eduki.Models.User({ email: $('#signup-field-email').val(), 
-                                            password: $('#signup-field-password').val() });
+    this.user = new Eduki.Models.User({ email: $('#signup-field-email').val(),
+                                        password: $('#signup-field-password').val() });
 
 		var self = this;
 
     // performs basic email validation
     // will alert user if email improperly filled out
     if (!this.user.isValid()) {
-  		$('#user-signup-form').append(this.errorTemplate());
+      if ($('.alert').length <= 0) {
+        $('#signup-field-email').after(this.errorTemplate());
+        $('.alert').delay(1000).fadeOut(function() {
+          $('.alert').remove();
+        });
+      }
   		return;
 		}
 
