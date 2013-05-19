b4efa5f2a0a3d880a98582d63ed450f9f2ae2e1c
Jolie Chen <omjolie@gmail.com>
2013-05-19 12:17:15 -0700

    Redefined Valid Email

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -10,8 +10,9 @@ Eduki.Models.User = Backbone.Model.extend({
 	// basic email validation. Checks to see if there is only 1 @ symbol, and if it is not the
 	// first character in the email.
 	validate: function(attrs, options) {
-		if (attrs.email.indexOf('@') <= 0 || attrs.email.indexOf('@') != attrs.email.lastIndexOf('@')) {
-			return 'Email not properly filled out';
+    var pattern = /.+@{1}.+\..+/;
+		if (!pattern.test(attrs.email)) {
+			return 'Please provide a valid email address';
 		}
 	}
 });
