a27d531088263583bf75d8366f4c23e8ce03ada0
Jolie Chen <omjolie@gmail.com>
2013-05-19 09:18:44 -0700

    Removed console.logs

--- app/assets/javascripts/models/course.js
+++ app/assets/javascripts/models/course.js
@@ -2,7 +2,6 @@ Eduki.Models.Course = Backbone.Model.extend({
   urlRoot: '/api/courses/',
 	validate : function (attrs, options) {
 		if (attrs.title == '') {
-      console.log('error');
 			return 'A title cannot be empty';
 		}
 	}
