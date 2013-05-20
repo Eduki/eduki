a94644ecd02a6dfec84b89155d3d493a2e671ff8
abboudm <abboud.mic@gmail.com>
2013-05-19 22:54:40 -0700

    added quiz creation server tests

--- spec/javascripts/views/quiz_new_spec.js
+++ spec/javascripts/views/quiz_new_spec.js
@@ -34,7 +34,33 @@ describe('Quiz', function() {
     });
 	});
 
-	describe('Adding', function() {
+	describe('New', function() {
+		var view;
+		beforeEach(function () {
+			setupFakeServer();
+			view = new Eduki.Views.QuizNew({attributes:{course_id: 1}});
+			view.$('#create-quiz-title').val('quizz');
+			view.$('#problem-0').val('problem');
+			view.$('.controls input[type=radio]:first-child').attr('checked', true);
+		});
+
+		it('saves a quiz and renders the button', function() {
+			view.submit();
+			serverRespond(this.server, 200, fixtures['quiz']);
+			expect(view.$el.find('.eduki-button-primary')).toHaveText('View Quiz');
+		});
+
+		it('saves a quiz and button has correct path', function() {
+			view.submit();
+			serverRespond(this.server, 200, fixtures['quiz']);
+			expect(view.$el.find('.eduki-button-primary').attr('href')).toEqual('/#/courses/1/quizzes/1');
+		});
+
+		it('fails to save a quiz and displays error message', function() {
+			view.submit();
+			serverRespond(this.server, 404, fixtures['quiz']);
+			expect(view.$el.find('h1')).toHaveText('Woops! Something went wrong.');
+		});
 
 	});
 });
