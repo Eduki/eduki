2a95f25a2018348b1f840546cf6e576b4cba53d8
abboudm <abboud.mic@gmail.com>
2013-05-19 23:09:08 -0700

    changed double quotes to singel quotes in courses_overview_spec

--- spec/javascripts/views/course_overview_spec.js
+++ spec/javascripts/views/course_overview_spec.js
@@ -8,10 +8,10 @@ describe('Course', function() {
   beforeEach(function() {
     currentUser.id = 1;
   });
-  describe("Overview", function() {
+  describe('Overview', function() {
     setupFakeServer();
 
-    it("renders error page", function() {
+    it('renders error page', function() {
       var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
       serverRespond(this.server, 301, fixtures['course']);
       serverRespond(this.server, 200, fixtures['quizzes']);
@@ -19,20 +19,20 @@ describe('Course', function() {
       expect(view.$el.find('h1')).toHaveText('Woops! Something went wrong.');
     });
 
-    it("renders course title", function() {
+    it('renders course title', function() {
       var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
       successServerResponses(this.server);
       expect(view.$el.find('h1').html()).toMatch('Bear Cooking');
     });
 
-    it("renders quiz create button", function() {
+    it('renders quiz create button', function() {
       var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
       successServerResponses(this.server);
       var buttons = view.$el.find('.eduki-button-primary span');
       expect(buttons[1]).toHaveText('create');
     });
 
-    it("renders lesson create button", function() {
+    it('renders lesson create button', function() {
       var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
       successServerResponses(this.server);
       var buttons = view.$el.find('.eduki-button-primary span');
@@ -40,28 +40,28 @@ describe('Course', function() {
     });
 
     // Tests for lessons
-    describe("Lessons", function() {
+    describe('Lessons', function() {
       it("renders lessons list", function() {
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
         successServerResponses(this.server);
         expect(view.$el.find('#course-lessons')).toContain('li');
       });
 
-      it("renders all lessons for that course", function() {
+      it('renders all lessons for that course', function() {
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
         successServerResponses(this.server);
         var lessons = view.$el.find('#course-lessons li a');
         expect(lessons.length).toBe(3);
       });
 
-      it("renders lessons title", function() {
+      it('renders lessons title', function() {
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
         successServerResponses(this.server);
         var lessons = view.$el.find('#course-lessons li a');
         expect(lessons[1]).toHaveText('Chopping Tongue');
       });
 
-      it("renders lessons link", function() {
+      it('renders lessons link', function() {
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
         successServerResponses(this.server);
         var lessons = view.$el.find('#course-lessons li a');
@@ -70,28 +70,28 @@ describe('Course', function() {
     });
 
     // Tests for quizzes
-      describe("Quizzes", function() {
-      it("renders quizzes list", function() {
+      describe('Quizzes', function() {
+      it('renders quizzes list', function() {
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
         successServerResponses(this.server);
         expect(view.$el.find('#course-quizzes')).toContain('li');
       });
 
-      it("renders all quizzes for that course", function() {
+      it('renders all quizzes for that course', function() {
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
         successServerResponses(this.server);
         var quizzes = view.$el.find('#course-quizzes li a');
         expect(quizzes.length).toBe(3);
       });
 
-      it("renders quiz title", function() {
+      it('renders quiz title', function() {
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
         successServerResponses(this.server);
         var quizzes = view.$el.find('#course-quizzes li a');
         expect(quizzes[1]).toHaveText('Quiz 2');
       });
 
-      it("renders quiz link", function() {
+      it('renders quiz link', function() {
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
         successServerResponses(this.server);
         var quizzes = view.$el.find('#course-quizzes li a');
@@ -100,13 +100,13 @@ describe('Course', function() {
     });
 
     describe('Enrollments', function() {
-      it("renders enroll button", function() {
+      it('renders enroll button', function() {
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 2}});
         successServerResponses(this.server);
         expect(view.$('#enroll span')).toHaveText('enroll');
       });
 
-      it("doesn't render button for user not logged in", function() {
+      it('doesn\'t render button for user not logged in', function() {
         console.log(currentUser.id);
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 2}});
         serverRespond(this.server, 200, fixtures['course']);
@@ -115,13 +115,13 @@ describe('Course', function() {
         expect(view.$('h1')).not.toContain('button');
       });
 
-      it("enrolled user", function() {
+      it('enrolled user', function() {
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 1}});
         successServerResponses(this.server);
         expect(view.$('#enroll span')).toHaveText('enrolled');
       });
 
-      it("enrolls user", function() {
+      it('enrolls user', function() {
         var view = new Eduki.Views.CoursesOverview({attributes:{course_id: 2}});
         successServerResponses(this.server);
         expect(view.$('#enroll span')).toHaveText('enroll');
