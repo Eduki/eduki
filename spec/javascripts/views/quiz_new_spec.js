ac3102945a8083f2cdff74a74d89d9a865d4b456
abboudm <abboud.mic@gmail.com>
2013-05-19 21:53:18 -0700

    added quiz_new_spec to test creating quizzes

---
+++ spec/javascripts/views/quiz_new_spec.js
@@ -0,0 +1,11 @@
+/*
+ * quiz_new_spec is a test suite for creating quizzes
+ *
+ * author: Michael
+ */
+
+describe('Quiz', function() {
+	describe('New', function() {
+		setupFakeServer();
+	});
+});
