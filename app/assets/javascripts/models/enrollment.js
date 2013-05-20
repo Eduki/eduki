fe31d74fcef9a1b244b066e1db97cd5a41eaed10
Jolie Chen <omjolie@gmail.com>
2013-05-19 21:23:00 -0700

    Enrollment URL

--- app/assets/javascripts/models/enrollment.js
+++ app/assets/javascripts/models/enrollment.js
@@ -2,11 +2,15 @@
  * Enrollment model
  *
  * author: Jolie Chen
- */
+*/
 
 Eduki.Models.Enrollment = Backbone.Model.extend({
   url: function() {
-    return '/api/enrollments/' + this.get('id');
+    // returns the appropriate url for the application of the quiz
+    if (this.get('user_id')) {
+      return '/api/users/' + this.get('user_id') + '/enrollments';
+    } else {
+      return '/api/enrollments/' + this.get('id');
+    }
   }
 });
-
