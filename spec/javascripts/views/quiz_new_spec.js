5602ddba575f9b7888884892e1bd537d6a38da5a
abboudm <abboud.mic@gmail.com>
2013-05-19 22:25:28 -0700

    added quiz creation rendering tests

--- spec/javascripts/views/quiz_new_spec.js
+++ spec/javascripts/views/quiz_new_spec.js
@@ -5,7 +5,36 @@
  */
 
 describe('Quiz', function() {
-	describe('New', function() {
-		setupFakeServer();
+
+	beforeEach(function() {
+    setupFakeServer();
+  });
+
+	describe('Render', function() {
+		it('renders title', function() {
+      var view = new Eduki.Views.QuizNew({attributes:{course_id: 1}});
+      expect(view.$el.find('h1').html()).toMatch('Create a Quiz');
+    });
+
+		it('renders quiz title input', function() {
+			var view = new Eduki.Views.QuizNew({attributes:{course_id: 1}});
+			expect(view.$el.find('#create-quiz-title')).toBeDefined();
+		});
+
+    it('renders first problem', function() {
+    	var view = new Eduki.Views.QuizNew({attributes:{course_id: 1}});
+      expect(view.$el.find('#create-quiz-problems')).toContain('fieldset');
+      expect(view.$el.find('.create-quiz-answer')).toContain('input[type=radio]');
+    });
+
+    it('renders submit button', function() {
+    	var view = new Eduki.Views.QuizNew({attributes:{course_id: 1}});
+    	var submit = view.$el.find('.eduki-button-primary');
+    	expect(submit[0]).toHaveText('Create Quiz');
+    });
+	});
+
+	describe('Adding', function() {
+
 	});
 });
