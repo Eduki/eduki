e6db561d54773f1ad32dc9eb964458a828967e63
Jolie Chen <omjolie@gmail.com>
2013-05-29 12:22:14 -0700

    Signup Form

--- app/assets/javascripts/views/static/static_index.js
+++ app/assets/javascripts/views/static/static_index.js
@@ -1,6 +1,7 @@
 Eduki.Views.StaticIndex = Backbone.View.extend({
 
   template: JST['static/index'],
+  splashTemplate: JST['static/splash'],
 
   initialize: function() {
     // nothing to initialize
@@ -8,6 +9,7 @@ Eduki.Views.StaticIndex = Backbone.View.extend({
 
   render: function() {
     $(this.el).html(this.template());
+    $('#main-content').before(this.splashTemplate());
     return this;
   },
 });
