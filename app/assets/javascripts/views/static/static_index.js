1f9eecdc80232394a6c50c47fc6746ee64443ca5
Jolie Chen <omjolie@gmail.com>
2013-05-29 17:28:16 -0700

    All Visual Components of Front Page

--- app/assets/javascripts/views/static/static_index.js
+++ app/assets/javascripts/views/static/static_index.js
@@ -1,9 +1,9 @@
 Eduki.Views.StaticIndex = Backbone.View.extend({
 
   template: JST['static/index'],
-  splashTemplate: JST['static/splash'],
-  loginTemplate: JST['static/splash_login'],
-  signupTemplate: JST['static/splash_signup'],
+  events: {
+    'click .toggle': 'toggleForm',
+  },
 
   initialize: function() {
     // nothing to initialize
@@ -11,7 +11,17 @@ Eduki.Views.StaticIndex = Backbone.View.extend({
 
   render: function() {
     $(this.el).html(this.template());
-    $(this.el).append(this.splashTemplate());
     return this;
+  },
+
+  toggleForm: function() {
+    var type = 'login';
+    if ($('form').attr('id') == 'login')
+      type = 'signup'
+
+    $('form').attr('id', type);
+    $('form h1').html(type);
+    $('button span').html(type);
+    $('.toggle').attr('id', 'toggle-' + type);
   },
 });
