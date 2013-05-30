4785934836a7713203f944bedb14092c5ba23ca4
Jolie Chen <omjolie@gmail.com>
2013-05-29 20:07:04 -0700

    User Login/Signup

--- app/assets/javascripts/views/static/static_index.js
+++ app/assets/javascripts/views/static/static_index.js
@@ -3,6 +3,8 @@ Eduki.Views.StaticIndex = Backbone.View.extend({
   template: JST['static/index'],
   events: {
     'click .toggle': 'toggleForm',
+    'submit form': 'submit',
+    'click input': 'hideInvalid',
   },
 
   initialize: function() {
@@ -23,5 +25,69 @@ Eduki.Views.StaticIndex = Backbone.View.extend({
     $('form h1').html(type);
     $('button span').html(type);
     $('.toggle').attr('id', 'toggle-' + type);
+    $('button[type=submit]').attr('id', 'button-' + type);
   },
+
+  // Validate user's credentials for valid email and non-empty password
+  submit: function(e) {
+    e.preventDefault();
+    this.user = new Eduki.Models.User({ email: $('#email').val(),
+                                        password: $('#password').val() });
+
+    // See if proper-formed credentials
+    if (!this.user.isValid()) {
+      this.showInvalid(this.user.validationError[0],
+                       this.user.validationError[1]);
+    } else if ($('form').attr('id') == 'signup') {
+      this.signup();
+    } else {
+      this.login();
+    }
+  },
+
+	// handles the form submission, displays appropriate pages on success/error
+  signup: function() {
+		var self = this;
+
+    // Saves user to database
+    this.user.save({email: this.user.get('email')},
+                   {wait: true,
+                    success: function() {
+                      self.login();
+                    },
+                    error: function(model, xhr, options) {
+                      if (xhr.status == 409)
+                        self.showInvalid('email', 'Email already exists');
+                      else
+                        self.render(self.errorTemplate());
+                    }});
+
+  },
+
+  hideInvalid: function() {
+    $('input').popover('hide');
+  },
+
+  showInvalid: function(input, message) {
+    $('#' + input).attr('data-content', message);
+    $('#' + input).popover('show');
+  },
+
+  login: function() {
+    currentUser.set_credentials(this.user.get('email'),
+                                this.user.get('password'));
+    currentUser.authenticate(this.onAuthenticateSuccess,
+                             this.onAuthenticateFailure, this);
+  },
+
+  onAuthenticateSuccess: function(data) {
+    this.hideLoading();
+    currentUser.save();
+    router.route("/dashboard");
+  },
+
+  onAuthenticateFailure: function(data) {
+    this.hideLoading();
+    router.route("/");
+  }
 });
