c9b9ed7deea62b0aac3023328d5541cd1f522596
David Mah <MahHaha@gmail.com>
2013-05-31 10:39:07 -0700

    Web Frontend Search
    * Added queryparams library
    * courses/search to search
    * courses not takes a queryparam that it also passes to the api. The
      search page is really a proxy that makes use of that query param
    * didn't do crap for tests trolololo

--- app/assets/javascripts/collections/courses.js
+++ app/assets/javascripts/collections/courses.js
@@ -1,5 +1,11 @@
 Eduki.Collections.Courses = Backbone.Collection.extend({
   model: Eduki.Models.Course,
-  url: '/api/courses'
+  url: '/api/courses',
 
+  // TODO: When search API is implemented, use whatever endpoint that gets
+  searchUrl: '/api/courses',
+
+  search: function(query) {
+    return this.fetch({data: query});
+  }
 });
---
+++ app/assets/javascripts/views/courses/courses_search.js
@@ -0,0 +1,27 @@
+Eduki.Views.CoursesSearch = Backbone.View.extend({
+  template: JST['courses/search'],
+  resultsTemplate: JST['courses/index'],
+  errorTemplate: JST['static/error'],
+
+  events: {
+    'submit form'  : 'search',
+    'click button' : 'search'
+  },
+
+  initialize: function() {
+    this.render(this.template());
+  },
+
+  render: function(template) {
+    $(this.el).html(template);
+    return this;
+  },
+
+  // A search is really a redirection to course index with a query in the
+  // request parameters
+  search: function(event) {
+    event.preventDefault();
+    query = {query: this.$el.find("#search-query").val()}
+    router.route("/courses", query);
+  },
+});
