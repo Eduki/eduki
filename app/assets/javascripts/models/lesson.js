68ca3d7d4b33d92c8b5f24251ba031a19e760c4d
Jolie Chen <omjolie@gmail.com>
2013-05-31 18:43:39 -0700

    Create lesson and tests

--- app/assets/javascripts/models/lesson.js
+++ app/assets/javascripts/models/lesson.js
@@ -1,9 +1,17 @@
 /*
  * Lesson model
  */
- 
+
 Eduki.Models.Lesson = Backbone.Model.extend({
   url: function() {
     return '/api/courses/' + this.get('course_id') + '/lessons/';
-  }
+  },
+
+	validate : function (attrs, options) {
+		if (!attrs.title) {
+			return new Array('create-lesson-title', 'Please provide a title');
+		} else if (!attrs.body) {
+			return new Array('create-lesson-body', 'Please provide lesson content');
+    }
+	}
 });
