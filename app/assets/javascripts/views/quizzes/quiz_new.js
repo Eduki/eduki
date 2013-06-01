2a7dde34d489e2b1e6493fd55ab6a58f356b40ad
Jolie Chen <omjolie@gmail.com>
2013-06-01 15:15:49 -0700

    Validate Quiz Before Submitting

--- app/assets/javascripts/views/quizzes/quiz_new.js
+++ app/assets/javascripts/views/quizzes/quiz_new.js
@@ -8,14 +8,13 @@ Eduki.Views.QuizNew = Backbone.View.extend({
 	
 	template: JST['quizzes/new'],
   problemTemplate: JST['quizzes/problem'],
-  deleteErrorTemplate: JST['quizzes/delete_error'],
 	errorTemplate: JST['static/error'],
-  createdTemplate: JST['quizzes/success'],
 
 	events: {
     'click #create-quiz-add' : 'add',
     'click .create-quiz-delete' : 'deleteProblem',
-		'click #submit': 'submit',
+		'click #submit': 'validateQuiz',
+    'click #create-quiz-title': 'hideInvalid',
     'click .create-quiz-question': 'hideInvalid',
     'click .create-quiz-answer': 'hideInvalid',
 	},
@@ -43,46 +42,64 @@ Eduki.Views.QuizNew = Backbone.View.extend({
       this.$(e.target).parent().remove();
     } else {
       this.showInvalid('.create-quiz-delete', 'A quiz must have at least one problem');
+      this.$('.create-quiz-delete').siblings('.popover').delay(2000).fadeOut();
     }
   },
 
-  submit: function() {
-    var problemsArray = new Array();
-    var submittedProblems = $('.create-quiz-question');
-    for (var i = 0; i < submittedProblems.length; i++) {
-      var problem = {
-        question: submittedProblems[i].value,
-        // the name being the submitted problems id is to handle the case where
-        // a user is creating a quiz and deletes problems that are in the middle
-        // by associating each question with its own 'problem-i' id, which corresponds
-        // a set of radio buttons for that question, it lets everything stay grouped
-        answer: $('input[name=' + submittedProblems[i].id + ']:checked', '#quiz').val()
-      };
-
-      // needs to have an error template for when they don't select an answer
-      if (!problem.answer) {
-        return;
-      }
-      problemsArray[i] = problem;
+  // Validate all fields are passed before submitting
+  validateQuiz: function() {
+    if (!this.$('#create-quiz-title').val()) {
+      console.log(this.$('#create-quiz-title').val());
+      this.showInvalid(this.$('form .control-label').first(), 'Please provide a title');
+      return;
     }
 
-    this.quiz = new Eduki.Models.Quiz({ course_id: this.attributes.course_id, 
-                                        title: $('#create-quiz-title').val(),
-                                        problems: problemsArray });
-    var self = this;
+    var submittedProblems = this.$('.create-quiz-problem');
+    var problems = new Array();
+    for (var i = 0; i < submittedProblems.length; i++) {
+      var questionLabel = $(submittedProblems[i]).find('.control-label').first();
+      var question = $(submittedProblems[i]).find('textarea');
+      var answer = this.$('input[name=' + $(question).attr('id') + ']:checked', '#quiz');
 
+      if (!(question.val() && answer.val())) {
+        this.showInvalid(questionLabel, 'Please provide a question and correct answer');
+      } else {
+        var problem = {
+          question: question.val(),
+
+          // the name being the submitted problems id is to handle the case where
+          // a user is creating a quiz and deletes problems that are in the middle
+          // by associating each question with its own 'problem-i' id, which corresponds
+          // a set of radio buttons for that question, it lets everything stay grouped
+          answer: answer.val()
+        }
+        problems[i] = problem;
+        }
+      }
+
+    console.log(submittedProblems.length + " " + problems.length)
+    if (submittedProblems.length == problems.length)
+      this.submit(problems);
+  },
+
+  // Submit quiz to database
+  submit: function(problems) {
+    this.quiz = new Eduki.Models.Quiz({ course_id: this.attributes.course_id,
+                                        title: $('#create-quiz-title').val(),
+                                        problems: problems});
+    // Save quiz to database
+    var self = this;
     $.when(this.quiz.save()).then(
-             function() { self.render(self.createdTemplate()); },
+             function() { router.route('/courses/' + self.quiz.get('course_id') +
+                                       '/quizzes/' + self.quiz.get('id'));},
              function() { self.render(self.errorTemplate()); }
            );
-
   },
 
   // Show an invalid message on error
   showInvalid: function(input, message) {
     this.$(input).attr('data-content', message);
     this.$(input).popover('show');
-    this.$('.create-quiz-delete').siblings('.popover').delay(3000).fadeOut();
   },
 
   // Hide validation error when input is clicked upon
