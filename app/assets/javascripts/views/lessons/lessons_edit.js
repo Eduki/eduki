9c24793fb677baec9d679ff362a82a470113d80c
abboudm <abboud.mic@gmail.com>
2013-06-01 14:30:11 -0700

    lesson editing functionality

---
+++ app/assets/javascripts/views/lessons/lessons_edit.js
@@ -0,0 +1,92 @@
+/*
+ * Handles editing page for lessons
+ *
+ * author: Michael
+ */
+
+Eduki.Views.LessonsEdit = Backbone.View.extend({
+
+  template: JST['lessons/edit'],
+  errorTemplate: JST['static/error'],
+
+  events: {
+  	'click button' : 'update',
+    'click #edit-lesson-title': 'hideInvalid',
+    'click #edit-lesson-body': 'hideInvalid'
+  },
+
+  initialize: function() {
+    // renders form, then updates all existing fields with the current profile values
+    // if they exist
+    this.lesson = new Eduki.Models.Lesson({ course_id: this.attributes.course_id,
+                                            id: this.attributes.lesson_id });
+    this.fetchLessonInfo();
+  },
+
+  // grabs current user info from database and displays in the form
+  // if it exists
+  fetchLessonInfo: function() {
+  	var self = this;
+  	this.lesson.fetch({
+      success: function() {
+      	// only updates fields if template is rendered correctly
+        if (self.render(self.template)) {
+        	self.updateFields();
+        }
+      },
+      error: function(model, xhr, options) {
+        self.render(self.errorTemplate)
+      }
+    });
+  },
+
+  // Renders the template only if user is logged in
+  // otherwise, routes them to the login page
+  render: function(template) {
+    if (currentUser.authenticated) {
+      $(this.el).html(template);
+      return this;
+    } else {
+      router.route('/login');
+      return false;
+    }
+  },
+
+  updateFields: function() {
+  	$('#edit-lesson-title').val(this.lesson.get('title'));
+    $('#edit-lesson-body').val(this.lesson.get('body'));
+  },
+
+  update: function() {
+  	this.lesson = new Eduki.Models.Lesson({ id: this.lesson.get('id'),
+  																					course_id: this.lesson.get('course_id'),
+                                            title: $('#edit-lesson-title').val(),
+                                            body: $('#edit-lesson-body'). val() });
+
+    // updates user info
+    // routes to dashboard on success
+    // renders error page on error
+    var self = this;
+    if (this.lesson.isValid()) {
+      this.lesson.save({id: this.lesson.get('id')},
+                     {wait: true,
+                      success: function() { router.route('/courses/' + self.lesson.get('course_id') 
+                      																	 + '/lessons/' + self.lesson.get('id')) },
+                      error: function() { self.render(self.errorTemplate()); }});
+    } else {
+      this.showInvalid(this.lesson.validationError[0],
+                       this.lesson.validationError[1]);
+    }
+  },
+
+  // Hide validation error when input is clicked upon
+  hideInvalid: function() {
+    this.$('input').popover('hide');
+  },
+
+  // Make the popoever appear with an error message
+  showInvalid: function(input, message) {
+    this.$('#' + input).attr('data-content', message);
+    this.$('#' + input).popover('show');
+  },
+});
