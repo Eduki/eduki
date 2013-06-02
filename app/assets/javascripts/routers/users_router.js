3f2630a714e44ffdc695987ddcc6b75bcf3186fb
Jolie Chen <omjolie@gmail.com>
2013-06-01 23:27:48 -0700

    Premium User Icon & Page

--- app/assets/javascripts/routers/users_router.js
+++ app/assets/javascripts/routers/users_router.js
@@ -1,16 +1,22 @@
 Eduki.Routers.Users = Backbone.Router.extend({
   routes: {
     'dashboard(/)': 'dashboard',
-    'user/update(/)': 'update'
+    'user/update(/)': 'update',
+    'user/premium(/)': 'premium',
   },
 
   dashboard: function() {
-    var view = new Eduki.Views.Dashboard
+    var view = new Eduki.Views.Dashboard;
     $('#main-content').html(view.render().el);
   },
 
-  update:function() {
-  	var view = new Eduki.Views.UpdateProfile
+  update: function() {
+  	var view = new Eduki.Views.UpdateProfile;
+  	$('#main-content').html(view.render().el);
+  },
+
+  premium: function() {
+  	var view = new Eduki.Views.Premium;
   	$('#main-content').html(view.render().el);
   }
 });
