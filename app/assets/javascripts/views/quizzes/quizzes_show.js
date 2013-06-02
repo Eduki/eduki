7624709a9665e634866e33b1956b07b1e66833f5
Jolie Chen <omjolie@gmail.com>
2013-06-01 19:43:53 -0700

    Submit Quiz Attempt

--- app/assets/javascripts/views/quizzes/quizzes_show.js
+++ app/assets/javascripts/views/quizzes/quizzes_show.js
@@ -9,8 +9,8 @@
    resultsTemplate: JST['quizzes/results'],
    errorTemplate: JST['static/error'],
    events: {
-     'click #submit-quiz': 'submit',
-     'submit form': 'submit',
+     'click #submit-quiz': 'grade',
+     'submit form': 'grade',
    },
 
   initialize: function() {
@@ -18,41 +18,73 @@
     this.course = new Eduki.Models.Course({id: this.attributes.course_id});
     this.quiz = new Eduki.Models.Quiz({id: this.attributes.quiz_id});
     this.quizzes = new Eduki.Collections.Quizzes({course_id: this.course.get('id')});
+    this.enrollments = new Eduki.Collections.Enrollments({user_id: currentUser.id});
 
     // Fetch course and lessons. Once retrieved, execute
     // render through the callback to display them.
     var self = this;
     $.when(this.course.fetch(),
            this.quizzes.fetch(),
-           this.quiz.fetch()).then(
-             function() { self.render(self.template()); },
-             function() { self.render(self.errorTemplate()); }
+           this.quiz.fetch(),
+           this.enrollments.fetch()).then(
+             function() {self.render(self.template());},
+             function() {self.render(self.errorTemplate());}
            );
   },
 
-  // Renders an individual lesson
+  // Renders an quiz unless user isn't logged in
   render: function(template) {
-    $(this.el).html(template);
-    return this;
-  },
-
-  submit: function() {
-    $('#quiz-results-modal').remove();
-    this.total = this.quiz.get('problems').length;
-    this.correct = this.grade();
-    this.$el.append(this.resultsTemplate());
-    $('#quiz-results-modal').modal();
-  },
-
-  grade: function() {
-    var correct = 0;
-    for (var i = 0; i < this.quiz.get('problems').length; i++) {
-      var inputAnswer = this.$('input:radio[name=problem-' +
-                          this.quiz.get('problems')[i].id + ']:checked').val();
-      var answer = this.quiz.get('problems')[i].answer;
-      if (inputAnswer == answer)
-        correct++;
+    if (currentUser.authenticated) {
+      $(this.el).html(template);
+      return this;
+    } else {
+      router.route('/');
+      return false;
     }
-    return correct;
+  },
+
+  // See if a user is enrolled
+  isEnrolled: function() {
+    this.enrollment = this.enrollments.findWhere({course_id: parseInt(this.course.get('id'))});
+    return this.enrollment;
+  },
+
+  // Saves quiz attempt
+  saveAttempt: function(problems) {
+    this.quizAttempt = new Eduki.Models.QuizAttempt({quiz_id: this.quiz.get('id'),
+                                                     enrollment_id: this.enrollment.get('id'),
+                                                     problem_attempts: problems});
+
+    var self = this;
+    this.quizAttempt.save({}, {
+      success: function() {
+        self.$el.append(self.resultsTemplate());
+        self.$('#quiz-results-modal').modal();
+      },
+      error: function() {self.render(self.errorTemplate());}});
+  },
+
+  // Calculate the total questions correct
+  // Create the problem attempts array to send to database
+  grade: function() {
+    if (this.isEnrolled()) {
+      var problemAttempts = new Array();
+      var correct = 0;
+      for (var i = 0; i < this.quiz.get('problems').length; i++) {
+        var inputAnswer = this.$('input:radio[name=problem-' +
+                          this.quiz.get('problems')[i].id + ']:checked').val();
+        var answer = this.quiz.get('problems')[i].answer;
+        if (inputAnswer == answer)
+          correct++;
+
+        // Empty string or user's answer is pushed onto problemAttempts
+        problemAttempts.push({answer: (inputAnswer ? inputAnswer : '')});
+      }
+      this.correct = correct;
+      this.saveAttempt(problemAttempts);
+    } else {
+      this.$('#submit-quiz').attr('data-content', 'Please enroll in course to take this quiz');
+      this.$('#submit-quiz').popover('show');
+    }
   },
 });
--- spec/javascripts/views/quiz_spec.js
+++ spec/javascripts/views/quiz_spec.js
@@ -1,11 +1,16 @@
 /*
  * quiz_spec describes a test suite for viewing and taking a quiz
- * 
+ *
  * author: Jolie Chen
  */
- 
+
 describe('Quiz', function() {
   describe("View", function() {
+    beforeEach(function() {
+      currentUser.id = 1;
+      currentUser.authenticated = true;
+    });
+
     setupFakeServer();
     it("renders error page for when an error occurs", function() {
       var view = new Eduki.Views.QuizShow({attributes:{course_id: 1, quiz_id: 1}});
@@ -34,6 +39,15 @@ describe('Quiz', function() {
       var possibleAnswers = view.$el.find('input');
       expect(problems.length).toBe(2);
       expect(possibleAnswers.length).toBe(8);
+    });
+
+    it("renders message if user is not enrolled", function() {
+      var view = new Eduki.Views.QuizShow({attributes:{course_id: 1, quiz_id: 1}});
+        serverRespond(this.server, 200, fixtures['course']);
+        serverRespond(this.server, 200, fixtures['quizzes']);
+        serverRespond(this.server, 200, fixtures['quiz']);
+        serverRespond(this.server, 200, {"id":1, "user_id":1, "course_id": 2});
+        expect(view.$el).toContain('.alert');
     });
 
     // Tests the other quiz section
@@ -89,19 +103,22 @@ describe('Quiz', function() {
       it("renders 0/0 correct answers for empty answers", function() {
         var view = new Eduki.Views.QuizShow({attributes:{course_id: 1, quiz_id: 1}});
         successServerResponses(this.server);
-        view.submit();
+        view.$('#submit-quiz').click();
+        serverRespond(this.server, 200, fixtures['quiz_attempt']);
         var score = view.$el.find('#quiz-results big');
+        expect(view.$el).not.toContain('.alert');
         expect(score).toHaveText("You got 0/2 questions correct");
       });
 
       it("renders 2/2 correct answers for correct answers", function() {
         var view = new Eduki.Views.QuizShow({attributes:{course_id: 1, quiz_id: 1}});
+        var problemAttempts = [{answer: "A"},{answer: "B"}];
         successServerResponses(this.server);
+        spyOn(view, 'saveAttempt');
         view.$('input[name=problem-1][value=A]').prop('checked', true);
         view.$('input[name=problem-2][value=B]').prop('checked', true);
-        view.submit();
-        var score = view.$el.find('#quiz-results big');
-        expect(score).toHaveText("You got 2/2 questions correct");
+        view.$('#submit-quiz').click();
+        expect(view.saveAttempt).toHaveBeenCalledWith(problemAttempts);
       });
 
       it("renders 1/2 correct answers for one correct and wrong answer", function() {
@@ -109,9 +126,54 @@ describe('Quiz', function() {
         successServerResponses(this.server);
         view.$('input[name=problem-1][value=A]').prop('checked', true);
         view.$('input[name=problem-2][value=C]').prop('checked', true);
-        view.submit();
+        view.$('#submit-quiz').click();
+        serverRespond(this.server, 200, fixtures['quiz_attempt']);
         var score = view.$el.find('#quiz-results big');
         expect(score).toHaveText("You got 1/2 questions correct");
+      });
+    });
+
+    describe('quiz attempt construction', function() {
+      it("creates properly all empty answers", function() {
+        var problemAttempts = [{answer: ""},{answer: ""}];
+        var view = new Eduki.Views.QuizShow({attributes:{course_id: 1, quiz_id: 1}});
+        spyOn(view, 'saveAttempt');
+        successServerResponses(this.server);
+        view.$('#submit-quiz').click();
+        expect(view.saveAttempt).toHaveBeenCalledWith(problemAttempts);
+      });
+
+      it("creates properly all filled answers", function() {
+        var view = new Eduki.Views.QuizShow({attributes:{course_id: 1, quiz_id: 1}});
+        var problemAttempts = [{answer: "A"},{answer: "B"}];
+        successServerResponses(this.server);
+        spyOn(view, 'saveAttempt');
+        view.$('input[name=problem-1][value=A]').prop('checked', true);
+        view.$('input[name=problem-2][value=B]').prop('checked', true);
+        view.$('#submit-quiz').click();
+        expect(view.saveAttempt).toHaveBeenCalledWith(problemAttempts);
+      });
+
+      it("creates properly incorrect answers", function() {
+        var problemAttempts = [{answer: "A"},{answer: "C"}];
+        var view = new Eduki.Views.QuizShow({attributes:{course_id: 1, quiz_id: 1}});
+        successServerResponses(this.server);
+        spyOn(view, 'saveAttempt');
+        view.$('input[name=problem-1][value=A]').prop('checked', true);
+        view.$('input[name=problem-2][value=C]').prop('checked', true);
+        view.$('#submit-quiz').click();
+        expect(view.saveAttempt).toHaveBeenCalledWith(problemAttempts);
+      });
+
+      it("creates properly incorrect answers", function() {
+        var problemAttempts = [{answer: "A"},{answer: "C"}];
+        var view = new Eduki.Views.QuizShow({attributes:{course_id: 1, quiz_id: 1}});
+        successServerResponses(this.server);
+        spyOn(view, 'saveAttempt');
+        view.$('input[name=problem-1][value=A]').prop('checked', true);
+        view.$('input[name=problem-2][value=C]').prop('checked', true);
+        view.$('#submit-quiz').click();
+        expect(view.saveAttempt).toHaveBeenCalledWith(problemAttempts);
       });
     });
   });
@@ -123,5 +185,6 @@ function successServerResponses(server) {
   serverRespond(server, 200, fixtures['course']);
   serverRespond(server, 200, fixtures['quizzes']);
   serverRespond(server, 200, fixtures['quiz']);
+  serverRespond(server, 200, fixtures['enrollments']);
 }
 
