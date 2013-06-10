753e2756ca09fa0e27437d9d3264b1f895f48c08
David Mah <MahHaha@gmail.com>
2013-06-09 21:32:21 -0700

    Changed question bodies to markdown

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -14,9 +14,11 @@
 # in order to be confirmed as a correct answer
 
 class Problem < ActiveRecord::Base
-  attr_accessible :question, :answer, :quiz, :quiz_id
+  attr_accessible :question, :question_markdown, :answer, :quiz, :quiz_id
   belongs_to :quiz
   has_many :problem_attempts, :dependent => :destroy
+
+  before_save :write_markdown
 
   # Given a HashWithIndifferentAccess containing :question and :answer
   # Return a Problem object. Does not save the record to the database
@@ -34,5 +36,11 @@ class Problem < ActiveRecord::Base
       :error => "Resource not found",
       :message => "Could not find Problem with ID=#{id}"
     }
+  end
+
+  # Parses question as markdown text and stores outputted html into
+  # self.question_markdown
+  def write_markdown
+    self.question_markdown = MarkdownWriter.format_as_markdown(self.question)
   end
 end
