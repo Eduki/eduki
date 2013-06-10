c701369167ae6d5208acdeaef517e6660cdae522
Jolie Chen <omjolie@gmail.com>
2013-06-09 17:14:40 -0700

    JSLint Footer

--- app/assets/javascripts/views/partials/footer.js
+++ app/assets/javascripts/views/partials/footer.js
@@ -1,3 +1,12 @@
+/* JSLint Arguments */
+/*jslint indent: 2*/
+/*jslint browser: true*/
+/*jslint vars: true*/
+/*jslint regexp: true*/
+/*global Eduki: false, Backbone: false, $: false, jQuery: false, currentUser: false,
+  JST: false, router: false */
+'use strict';
+
 /* This view encapsulates logic for the logic partial
  *
  * author: Jolie Chen
@@ -5,7 +14,7 @@
 Eduki.Views.Footer = Backbone.View.extend({
   template: JST['partials/footer'],
 
-  render: function() {
+  render: function () {
     $(this.el).html(this.template());
     return this;
   },
